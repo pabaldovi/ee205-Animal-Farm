5f31a1d5aed807ba45340514cbff593c93104cdd
paulobaldovi <baldovipaulo@gmail.com>
2022-04-10 10:03:43 -1000

    Added Cat class

---
+++ Cat.cpp
@@ -0,0 +1,11 @@
+///////////////////////////////////////////////////////////////////////////////
+///         University of Hawaii, College of Engineering
+/// @brief  ee205_lab_08d_animal_farm_1_to_clion - EE 205 - Spr 2022
+///
+/// @file Cat.cpp
+/// @version 1.0
+///
+/// @author Paulo Baldovi <pbaldovi@hawaii.edu>
+/// @date   10_Apr_2022
+///////////////////////////////////////////////////////////////////////////////
+#include "Cat.h"
---
+++ Cat.h
@@ -0,0 +1,20 @@
+///////////////////////////////////////////////////////////////////////////////
+///         University of Hawaii, College of Engineering
+/// @brief  ee205_lab_08d_animal_farm_1_to_clion - EE 205 - Spr 2022
+///
+/// @file Cat.h
+/// @version 1.0
+///
+/// @author Paulo Baldovi <pbaldovi@hawaii.edu>
+/// @date   10_Apr_2022
+///////////////////////////////////////////////////////////////////////////////
+#ifndef EE205_LAB_08D_ANIMAL_FARM_1_TO_CLION_CAT_H
+#define EE205_LAB_08D_ANIMAL_FARM_1_TO_CLION_CAT_H
+
+
+class Cat {
+
+};
+
+
+#endif //EE205_LAB_08D_ANIMAL_FARM_1_TO_CLION_CAT_H
