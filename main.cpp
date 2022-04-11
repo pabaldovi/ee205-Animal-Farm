bbae55b5a40786898c9f05da9967a82892b599a8
paulobaldovi <baldovipaulo@gmail.com>
2022-04-10 20:50:03 -1000

    Implemented main() from ref repo

--- main.cpp
+++ main.cpp
@@ -9,48 +9,143 @@
 /// @date   20_Mar_2022
 ///////////////////////////////////////////////////////////////////////////////
 
+#include <cstdlib>  // For EXIT_SUCCESS / EXIT_FAILURE
+#include <cassert>  // For assert()
+#include <cstring>  // For strcmp()
+#include <exception>  // For try/catch blocks
 #include <iostream>
+
+#include "config.h"
+#include "Cat.h"
 #include "catDatabase.h"
 #include "addCats.h"
-#include "deleteCats.h"
 #include "reportCats.h"
-#include "updateCats.h"
-#include "config.h"
-//#include "Cat.h"
+#include "deleteCats.h"
 
-using namespace std;
+using namespace std ;
 
+/// @internal Used to test the largest possible name
+#define MAX_NAME1    "1234567890123456789012345678901234567890123456789"
+
+/// @internal Used to test an illegal name (because it's too large by
+///           one character)
+#define ILLEGAL_NAME "12345678901234567890123456789012345678901234567890"
+
+
+/// The entry point for Animal Farm
 int main() {
-   cout << "Starting " << PROGRAM_NAME << endl;
+    cout << "Starting " << PROGRAM_NAME << endl ;
 
-   addCat( new Cat("Loki",   Cat::MALE,     Cat::PERSIAN,    1.0 )) ;
-   addCat( new Cat("Milo",   Cat::MALE,     Cat::MANX,       1.1 )) ;
-   addCat( new Cat("Bella",  Cat::FEMALE,   Cat::MAINE_COON, 1.2 )) ;
-   addCat( new Cat("Kali",   Cat::FEMALE,   Cat::SHORTHAIR,  1.3 )) ;
-   addCat( new Cat("Trin",   Cat::FEMALE,   Cat::MANX,       1.4 )) ;
-   addCat( new Cat("Chili",  Cat::MALE,     Cat::SHORTHAIR,  1.5)) ;
-   printAllCats();
-   deleteAllCats();
-   printAllCats();
-   /*
-   int kali = findCat( "Kali" ) ;
-   updateCatName( kali, "Chili" ) ; // this should fail
-   printCat( kali );
-   updateCatName( kali, "Capulet" ) ;
-   updateCatWeight( kali, 9.9 ) ;
-   updateCatCollar1(kali, WHITE);
-   updateCatCollar2(kali, WHITE);
-   updateLicense(kali, 123);
-   fixCat( kali ) ;
-   printCat( kali );
-   printAllCats();
-   deleteAllCats();
-   addCat( "Loki",   MALE,             PERSIAN,    true,    8.5,  BLACK, WHITE, 101 ) ;
-   deleteCat(0);
-   printAllCats();
-   //addCat( "Loki", MALE, PERSIAN, true, 8.5 ) ;
-   //addCat( "Loki", MALE, PERSIAN, true, 8.5 ) ;*/
+//    initializeDatabase() ;
 
-   cout << "Done with " << PROGRAM_NAME << endl;
-   return 0;
+#ifdef DEBUG
+    {
+      // Verify that a cat's default values are set
+      Cat testCat = Cat();
+      assert(testCat.getName() != nullptr );
+      assert(strcmp(testCat.getName(), "") == 0);
+      assert(testCat.getGender() == UNKNOWN_GENDER);
+      assert(testCat.getBreed() == UNKNOWN_BREED);
+      assert(testCat.isFixed() == false);
+      assert(testCat.getWeight() == UNKNOWN_WEIGHT);
+      assert(!testCat.isFixed());
+      assert(!testCat.validate());  // The default cat is invalid
+
+      // Test for NULL name
+      try {
+         testCat.setName(nullptr);
+         assert(false); // We should never get here
+      } catch (exception const &e) {}
+
+      // Test for empty name
+      try {
+         testCat.setName("");
+         assert(false); // We should never get here
+      } catch (exception const &e) {}
+
+      // Test valid names
+      testCat.setName("A");       // A 1 character name is valid
+      testCat.setName(MAX_NAME1); // A MAX_NAME1 name is valid
+
+      // Test for name too large
+      try {
+         testCat.setName(ILLEGAL_NAME);
+         assert(false); // We should never get here
+      } catch (exception const &e) {}
+
+      testCat.setGender(FEMALE);
+
+      try {
+         testCat.setGender(MALE);
+         assert(false); // We should never get here
+      } catch (exception const &e) {}
+
+      testCat.setBreed(MAINE_COON);
+
+      try {
+         testCat.setBreed(MANX);
+         assert(false); // We should never get here
+      } catch (exception const &e) {}
+
+      testCat.fixCat();
+      assert(testCat.isFixed());
+
+      // Test for Weight <= 0
+      try {
+         testCat.setWeight(0);
+         assert(false); // We should never get here
+      } catch (exception const &e) {}
+
+      testCat.setWeight(1.0 / 1024);
+      assert(testCat.getWeight() == 1.0 / 1024);
+
+      assert(testCat.validate());  // The cat should now be valid
+      testCat.print() ;
+
+      assert(!isCatInDatabase(&testCat)) ;
+   }
+#endif
+
+    bool result ;
+    result = addCat( new Cat( "Loki", Cat::MALE, Cat::PERSIAN, 1.0 )) ;
+    assert( result ) ;
+    if( !result ) throw logic_error (PROGRAM_NAME ": addCat() failed" ) ;
+    result = addCat( new Cat( "Milo", Cat::MALE, Cat::MANX , 1.1 )) ;
+    assert( result ) ;
+    result = addCat( new Cat( "Bella", Cat::FEMALE, Cat::MAINE_COON, 1.2 )) ;
+    assert( result ) ;
+    result = addCat( new Cat( "Kali", Cat::FEMALE, Cat::SHORTHAIR, 1.3 )) ;
+    assert( result ) ;
+    result = addCat( new Cat( "Trin", Cat::FEMALE, Cat::MANX, 1.4 )) ;
+    assert( result ) ;
+    result = addCat( new Cat( "Chili", Cat::MALE, Cat::SHORTHAIR, 1.5 )) ;
+    assert( result ) ;
+
+#ifdef DEBUG
+    {
+      // Test finding a cat...
+      Cat *bella = findCatByName("Bella");
+      assert(bella != nullptr);
+      // Test not finding a cat
+      assert(findCatByName("Bella's not here") == nullptr);
+
+      // Test deleting a cat...
+      assert(deleteCat(bella) == true);
+      try {
+         deleteCat(bella); // Verify that Bella's not there
+      } catch (exception const &e) {}
+
+      bella = nullptr;
+   }
+#endif
+
+    printAllCats() ;
+
+    deleteAllCats() ;
+
+    printAllCats() ;
+
+    cout << "Done with " << PROGRAM_NAME ;
+
+    return( EXIT_SUCCESS ) ;
 }
