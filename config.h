ab88a4ccf4fc49ea8a5b2f78ee4578cfcffbc3f0
paulobaldovi <baldovipaulo@gmail.com>
2022-04-24 13:21:24 -1000

    Added macros to print dump heading and format line

--- config.h
+++ config.h
@@ -11,4 +11,20 @@
 
 #pragma once
 
-#define PROGRAM_NAME "Animal Farm 2"
+#include <iostream>
+
+#define PROGRAM_TITLE "Animal Farm 3"
+#define PROGRAM_NAME "animalfarm3"
+
+#define PRINT_HEADING_FOR_DUMP                                              \
+    /* Print =========================================================== */ \
+    std::cout << std::setw(80) << std::setfill( '=' ) << "" << std::endl
+
+
+#define FORMAT_LINE_FOR_DUMP(className, member)                             \
+    std::cout << std::setfill( ' ' )  /* Space pad    */                    \
+              << std::left            /* Left justify */                    \
+              << std::boolalpha  /* Print `true` or `false` for `bool`s */  \
+              << std::setw( 8) << (className)                               \
+              << std::setw(20) << (member)                                  \
+              << std::setw(52)  /* (data) */
