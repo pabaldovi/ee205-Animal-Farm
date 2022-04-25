3d51970929024537c3e07adf1b88430d408b8c6f
paulobaldovi <baldovipaulo@gmail.com>
2022-04-24 23:20:20 -1000

    Updated header comments

--- Color.cpp
+++ Color.cpp
@@ -1,6 +1,6 @@
 ///////////////////////////////////////////////////////////////////////////////
 ///         University of Hawaii, College of Engineering
-/// @brief  ee205_lab_08d_animal_farm_1_to_clion - EE 205 - Spr 2022
+/// @brief  ee205_lab_13a_animal_farm_3 - EE 205 - Spr 2022
 ///
 /// @file Color.cpp
 /// @version 1.0
