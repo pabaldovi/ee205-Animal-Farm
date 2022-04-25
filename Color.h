e061784710bc8a9b14b266d5308fc7225aee5556
paulobaldovi <baldovipaulo@gmail.com>
2022-04-25 00:09:09 -1000

    Moved function definition to header file and removed source file

--- Color.h
+++ Color.h
@@ -12,6 +12,7 @@
 #pragma once
 
 #include <ostream>
+#include "config.h"
 
 enum class Color {
     UNKNOWN_COLOR,
@@ -27,4 +28,44 @@ enum class Color {
     GINGER
 };
 
-inline std::ostream& operator<<( std::ostream& lhs_stream, const Color& rhs_Color );
+inline std::ostream& operator<<( std::ostream& lhs_stream, const Color& rhs_Color ) {
+    switch( rhs_Color ) {
+        case Color::UNKNOWN_COLOR:
+            lhs_stream << "Unknown color";
+            break;
+        case Color::BLACK:
+            lhs_stream << "Black";
+            break;
+        case Color::WHITE:
+            lhs_stream << "White";
+            break;
+        case Color::RED:
+            lhs_stream << "Red";
+            break;
+        case Color::BLUE:
+            lhs_stream << "Blue";
+            break;
+        case Color::GREEN:
+            lhs_stream << "Green";
+            break;
+        case Color::PINK:
+            lhs_stream << "Pink";
+            break;
+        case Color::CREAM:
+            lhs_stream << "Cream";
+            break;
+        case Color::BROWN:
+            lhs_stream << "Brown";
+            break;
+        case Color::CALICO:
+            lhs_stream << "Calico";
+            break;
+        case Color::GINGER:
+            lhs_stream << "Ginger";
+            break;
+        default:
+            /// @throw out_of_range If the enum is not mapped to a string.
+            throw std::out_of_range( PROGRAM_NAME ": Color not mapped to a string" );
+    }
+    return lhs_stream;
+}
